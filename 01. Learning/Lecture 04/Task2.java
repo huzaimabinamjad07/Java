4aab5e985967c877e74824efdd80be7f885892e6
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-15 21:02:56 +0500

    progress...

---
+++ 01. Learning/Lecture 04/Task2.java
@@ -0,0 +1,7 @@
+public class Task2 {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("I'll become one of the best programmer");
+        }
+    }
+}
