a2d81a7e3fb2770869e9c904ea1498f58020d2b3
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-15 11:40:03 +0500

    initial commit

--- 01 - Basic Exercises Part 1/Task16.java
+++
@@ -1,9 +0,0 @@
-public class Task16 {
-    public static void main(String[] args) {
-        System.out.println(" +\"\"\"\"\"+");
-        System.out.println("[| o o |]");
-        System.out.println(" |  ^  |");
-        System.out.println(" | '-' |");
-        System.out.println(" +-----+");
-    }
-}
--- 01 - Basic Exercises Part 1/Task20.java
+++
@@ -1,5 +0,0 @@
-public class Task20 {
-    public static void main(String[] args) {
-        
-    }
-}
