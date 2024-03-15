478cdad8bdd9881cb231d250be1d93dd90878545
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-15 21:42:59 +0500

    progress...

---
+++ 01. Learning/Lecture 05/Task5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Todo: Pattern (Diamond Top Left)
+
+        System.out.print("Enter rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int spc = n - i;
+
+            for (int j = 1; j <= spc; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
