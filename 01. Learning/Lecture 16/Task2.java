5424e5d83e8a3b8313ccf14d657fcf395fff52c0
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-16 17:12:16 +0500

    progress...

---
+++ 01. Learning/Lecture 16/Task2.java
@@ -0,0 +1,36 @@
+public class Task2 {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 5, 1 };
+
+        System.out.println("===== Before Sort =====");
+        print(arr);
+
+        selectionSort(arr);
+
+        System.out.println("===== After Sort =====");
+        print(arr);
+    }
+}
