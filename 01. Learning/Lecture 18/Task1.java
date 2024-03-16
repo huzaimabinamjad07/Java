02ac391a00d224c223c272151acfdadaed6385fa
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-17 03:58:42 +0500

    progress...

---
+++ 01. Learning/Lecture 18/Task1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void toh(int n, String s, String h, String d) {
+        if (n == 1) {
+            System.out.println("Transfer " + n + " from " + s + " to " + d);
+            return;
+        }
+
+        toh(n - 1, s, d, h);
+
+        System.out.println("Transfer " + n + " from " + s + " to " + d);
+
+        toh(n - 1, h, s, d);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter disks: ");
+        int n = sc.nextInt();
+
+        toh(n, "Source", "Helper", "Destination");
+
+        sc.close();
+    }
+}
